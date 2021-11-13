38c26ff230f5d7c6b7907804626d4ee8033a742c
DaurKas <idaurenkasengaliev@gmail.com>
2021-11-13 14:42:17 +0300

    testing git

---
+++ include/const.h
@@ -0,0 +1,16 @@
+#ifndef CONST_H 
+#define CONST_H
+#include <cmath>
+using namespace std;
+extern const int N;
+extern const double BETA1;
+extern const double BETA2;
+extern const double MU1;
+extern const double MU2;
+extern const double EPS;
+double k(double x);
+
+double q(double x);
+
+double f(double x);
+#endif
